901a0e12055a0854efa7d36896dd0cc8dcbd259b
katta kirankumar <kumarkatta916@gmail.com>
2026-04-21 16:21:52 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  features {}
-
- subscription_id = "90a3a94a-bedd-4043-8c36-56c5dcddb8de"
-  client_id       = "51ce7594-dcf5-4a6c-b8ad-01574986a952"
-  client_secret   = ".ku8Q~BgMObP8qMJhGdb8Lo-Rnunk6qR.XsC2b.O"
-  tenant_id       = "e9f17581-2abe-4748-bd7b-260b605eceda"
-}
